4014595ba7a03d8c0dba40cd85d0b9a8bb69e18d
arpit <arpitsahu789@gmail.com>
2023-05-03 20:41:46 +0530

    done with placing circular progress indicator

---
+++ lib/progress_indicator.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:get/get.dart';
+
+class CircularIndicator extends StatefulWidget {
+  double value;
+  CircularIndicator({Key? key,required this.value}) : super(key: key);
+
+  @override
+  State<CircularIndicator> createState() => _CircularIndicatorState();
+}
+
+class _CircularIndicatorState extends State<CircularIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return  SleekCircularSlider(
+      initialValue: widget.value,
+      max: 100,
+      min: 0,
+      appearance:  CircularSliderAppearance(
+          angleRange: 360,
+          startAngle: 0,
+          size: Get.width * 0.4,
+          customWidths: CustomSliderWidths(
+              trackWidth: 20,
+              progressBarWidth: 20,
+              shadowWidth: 0,
+              handlerSize: 0
+          ),
+          customColors: CustomSliderColors(
+            hideShadow: true,
+            gradientStartAngle: 0,
+            trackColor: const Color(0xffEEEEEE),
+            progressBarColors: [
+              const Color(0xffFFC837),
+              const Color(0xffFF8008),
+            ],
+          )
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:futurista_edu/extensions/extensions.dart';
+import 'package:futurista_edu/progress_indicator.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,12 +14,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: SingleChildScrollView(
-      child: Column(
-        children: [
-
-        ],
-      ),
-    ));
+    return Scaffold(
+      body: SafeArea(child: SingleChildScrollView(
+        child: Column(
+          children:  [
+            SizedBox(height: 5.0.hp,),
+            Center(child: CircularIndicator(value: 48,))
+          ],
+        ),
+      )),
+    );
   }
 }
